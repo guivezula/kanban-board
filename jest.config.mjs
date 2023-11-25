2c9060c3ed50ff176c6b0de40fb0064a84734b3b
Guilherme Vezula <guilherme.vezula@meiuca.design>
2023-11-25 11:45:48 -0300

    install dependencies

---
+++ jest.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
