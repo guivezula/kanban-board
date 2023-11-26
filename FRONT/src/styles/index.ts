c62911873e0af6240989066bf95cf76fd625108b
Guilherme Vezula <guilherme.vezula@meiuca.design>
2023-11-26 09:03:06 -0300

    add styles

---
+++ FRONT/src/styles/index.ts
@@ -0,0 +1,17 @@
+export const styles = {
+  color: {
+    grey: "rgb(24, 28, 35)",
+    green: "rgb(166, 247, 80)",
+    white: "rgb(255, 251, 240)",
+  },
+  font: {
+    family: {
+      title: "Ubuntu, sans-serif",
+      form: "Fira Code, monospace",
+    },
+    size: {
+      title: "16px",
+      form: "14px",
+    },
+  },
+};
